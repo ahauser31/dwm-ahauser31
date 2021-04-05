f6f3f393d55da8f48ba65565e8da1af8d1b19fb2
Andreas Hauser <andreas.hauser@vde.com>
2021-04-05 15:07:05 +0800

    Bugfix

--- config.def.h
+++ config.def.h
@@ -1,7 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "vanitygaps.c"
-
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 
 /* appearance */
@@ -46,6 +44,8 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+
+#include "vanitygaps.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
