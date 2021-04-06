e6201d3385366267604f7a4719e321c08a8bea8e
Andreas Hauser <andreas.hauser@vde.com>
2021-04-07 00:55:46 +0800

    Configured autostart

--- config.def.h
+++ config.def.h
@@ -26,8 +26,8 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"sxhkd", NULL,
-  "picom", "-b", "--config", "~/.config/picom/picom.conf", NULL,
-	"wallpaper_reddit", "|", "xargs", "hsetroot", "-cover", NULL,
+  "picom", "-b", NULL,
+	"/bin/sh", "-c", "$HOME/.local/scripts/wallpaper_reddit | xargs hsetroot -cover", NULL,
 	NULL /* terminate */
 };
 
